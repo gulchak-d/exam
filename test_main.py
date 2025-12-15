fc7aa109a302c9c3acacba85bbe486f5213d158a
gulchak-d <gulchak@knu.ua>
2025-12-15 16:24:30 +0200

    commit

--- test_main.py
+++ test_main.py
@@ -10,11 +10,9 @@ class TestFibonacci(unittest.TestCase):
         self.assertEqual(fibonacci(1), 1)
         
     def test_positive_number(self):
-        # 10-й елемент Фібоначчі це 55
         self.assertEqual(fibonacci(10), 55)
 
     def test_negative_input(self):
-        # Перевірка на генерацію виключення
         with self.assertRaises(ValueError):
             fibonacci(-5)
 
